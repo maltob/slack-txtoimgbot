6bf5e5995c77d303a840f69ec9409590aed90c40
Michael Altobelli <maltob@users.noreply.github.com>
2022-10-17 22:08:17 -0400

    Lock to prevent running multiple generations at a time Add counter for roughly how long when multiple generations are pending.

---
+++ tsCounter.py
@@ -0,0 +1,20 @@
+from threading import Lock
+class tsCounter:
+    def __init__(self) -> None:
+        self.lock = Lock()
+        self.counterValue = 0
+    def getValue() -> int:
+        return self.counterValue
+    def increment(self):
+        try:
+            self.lock.acquire()
+            self.counterValue = self.counterValue + 1
+        finally:
+            self.lock.release()
+    def decrement(self):
+        try:
+            self.lock.acquire()
+            self.counterValue = self.counterValue - 1
+        finally:
+            self.lock.release()
+        
